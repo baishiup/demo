8a1d33d91d20f4f8b8b2b34c48e440f4f3c741e6
baishiup <415453941@qq.com>
2019-01-29 11:59:56 +0800

    fix:解决406

--- src/main/java/com/hyq/demo/HelloWorldController.java
+++ src/main/java/com/hyq/demo/HelloWorldController.java
@@ -5,19 +5,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class HelloWorldController extends Base {
 
-  @RequestMapping(value="helloworld")
+  @RequestMapping(value = "helloworld")
   @ResponseBody
-  public User helloWorld() {
-//    Result res = new Result();
-
-    User res = new User();
+  public Result helloWorld() {
+    List<Result> list = new ArrayList<>();
+    list.add(new Result());
+    list.add(new Result());
+    list.add(new Result());
+    list.add(new Result());
+    Result res = new Result();
+    res.setData(list);
     return res;
-  }
-
-  public class User{
-    String name = "asdasd";
   }
 }
--- src/main/java/com/hyq/demo/controller/Base.java
+++ src/main/java/com/hyq/demo/controller/Base.java
@@ -1,10 +1,37 @@
 package com.hyq.demo.controller;
 
+import java.util.List;
+
 public class Base {
 
   // 默认输出格式
   public class Result {
-    String message = "ok";
-    int state = 0;
+    private String message = "ok";
+    private List data = null;
+    private int state = 0;
+
+    public String getMessage() {
+      return message;
+    }
+
+    public void setMessage(String message) {
+      this.message = message;
+    }
+
+    public List getData() {
+      return data;
+    }
+
+    public void setData(List data) {
+      this.data = data;
+    }
+
+    public int getState() {
+      return state;
+    }
+
+    public void setState(int state) {
+      this.state = state;
+    }
   }
 }
